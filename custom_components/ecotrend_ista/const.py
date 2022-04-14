2587638cd2ede82b3dc8e0a43ceec5ac7a343893
Ludy87 <Ludy87@users.noreply.github.com>
2022-04-14 11:16:32 +0200

    Create const.py

---
+++ custom_components/ecotrend_ista/const.py
@@ -0,0 +1,10 @@
+DOMAIN = "ecotrend_ista"
+
+DEFAULT_SCAN_INTERVAL = 3 * 60 * 10
+
+CONF_CONTROLLER = "controller"
+CONF_EMAIL = "email"
+CONF_PASSWORD = "password"
+
+CONF_TYPE_HEATING = "heating"
+CONF_TYPE_HEATWATER = "warmwater"
