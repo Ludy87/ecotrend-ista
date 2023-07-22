abf01660ca99c0f2835c2157f2dceeb21257b028
Ludy <Ludy87@users.noreply.github.com>
2023-07-22 17:40:52 +0200

    V2.1.0 (#89)
    * Create diagnostics.py
    
    * Update manifest.json
    
    * Update manifest.json

---
+++ custom_components/ecotrend_ista/diagnostics.py
@@ -0,0 +1,16 @@
+"""Diagnostics support for YouTube."""
+from __future__ import annotations
+
+from typing import Any
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+from .coordinator import IstaDataUpdateCoordinator
+
+
+async def async_get_config_entry_diagnostics(hass: HomeAssistant, entry: ConfigEntry) -> dict[str, Any]:
+    """Return diagnostics for a config entry."""
+    coordinator: IstaDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+    return coordinator.data
