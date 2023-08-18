98e56c94dc26fdc380d5eb2c4377d476de0e654f
Ludy87 <Ludy87@users.noreply.github.com>
2023-08-18 10:04:28 +0200

    Update config_flow.py

--- custom_components/ecotrend_ista/config_flow.py
+++ custom_components/ecotrend_ista/config_flow.py
@@ -1,12 +1,14 @@
 """Config flow for ista EcoTrend Version 2."""
 from __future__ import annotations
 
+import copy
 import logging
 from typing import Any
 
-import voluptuous as vol
 from pyecotrend_ista.exception_classes import LoginError
 from pyecotrend_ista.pyecotrend_ista import PyEcotrendIsta
+import requests
+import voluptuous as vol
 
 from homeassistant import config_entries, core
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD
@@ -17,7 +19,7 @@ from homeassistant.helpers.selector import (
     NumberSelectorMode,
 )
 
-from .const import CONF_UPDATE_INTERVAL, CONF_URL, DOMAIN, MANUFACTURER
+from .const import CONF_MFA, CONF_UPDATE_INTERVAL, CONF_URL, DOMAIN, MANUFACTURER
 from .const_schema import DATA_SCHEMA_EMAIL, URL_SELECTOR
 
 _LOGGER = logging.getLogger(__name__)
@@ -25,22 +27,23 @@ _LOGGER = logging.getLogger(__name__)
 
 @staticmethod
 @core.callback
-def login_account(hass: core.HomeAssistant, data, demo: bool = False) -> PyEcotrendIsta:
+def login_account(hass: core.HomeAssistant, data: dict, demo: bool = False) -> PyEcotrendIsta:
     """Log into an Ecotrend-Ista account and return an account instance."""
     account = PyEcotrendIsta(
         email=data.get(CONF_EMAIL, None),
         password=data.get(CONF_PASSWORD, None),
         logger=_LOGGER,
         hass_dir=(hass.config.path("custom_components/ecotrend_ista") if demo else None),
+        totp=data.get(CONF_MFA, "").replace(" ", ""),
+        session=requests.Session(),
     )
-
     return account
 
 
 async def validate_input(hass: core.HomeAssistant, data: dict[str, Any]) -> dict[str, str]:
     """Validate the user input allows us to connect.
     Data has the keys from DATA_SCHEMA_EMAIL with values provided by the user.
-    """
+    """  # noqa: D205
     if CONF_URL not in data or data[CONF_URL] != "de_url":
         raise NotSupportedURL()
 
@@ -48,15 +51,25 @@ async def validate_input(hass: core.HomeAssistant, data: dict[str, Any]) -> dict
     try:
         vol.Email()(data.get(CONF_EMAIL))
     except vol.Invalid as error:
-        raise vol.Invalid(error)
+        raise vol.Invalid(error) from error
 
     account = login_account(hass, data)
 
     login_info = None
     try:
-        login_info = await account.login()
+        login_info = await hass.async_add_executor_job(account.login)
     except LoginError as error:
+        _LOGGER.error(error)
         raise LoginError(error) from error
+    except requests.ConnectionError as error:
+        _LOGGER.error(error)
+        raise requests.ConnectionError from error
+    except requests.ReadTimeout as error:
+        _LOGGER.error(error)
+        raise requests.ReadTimeout from error
+    except requests.Timeout as error:
+        _LOGGER.error(error)
+        raise requests.Timeout from error
 
     return {
         "title": f"{MANUFACTURER} {account.getSupportCode()} {'' if not login_info or login_info != 'Demo' else login_info}"
@@ -87,7 +100,6 @@ class IstaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_german(self, user_input: dict[str, Any] | None = None) -> FlowResult:
         """Handle the initial step."""
         errors: dict[str, str] = {}
-
         if user_input is not None:
             unique_id = f"{user_input[CONF_EMAIL]}"
             await self.async_set_unique_id(unique_id)
@@ -103,6 +115,12 @@ class IstaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 errors["base"] = "not_allowed"
             except vol.Invalid:
                 errors["base"] = "no_email"
+            except requests.ConnectionError:
+                errors["base"] = "cannot_connect"
+            except (requests.ReadTimeout, requests.Timeout):
+                errors["base"] = "timeout_connect"
+            except Exception:  # pylint: disable=broad-except
+                errors["base"] = "unknown"
 
             if info:
                 return self.async_create_entry(
@@ -110,6 +128,7 @@ class IstaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                     data={
                         CONF_EMAIL: user_input[CONF_EMAIL],
                         CONF_PASSWORD: user_input[CONF_PASSWORD],
+                        CONF_MFA: user_input.get(CONF_MFA, "").replace(" ", ""),
                     },
                     options={
                         CONF_URL: user_input[CONF_URL],
@@ -128,9 +147,8 @@ class IstaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_import(self, import_data: dict[str, Any]):
         """Import ista EcoTrend Version 2 config from configuration.yaml."""
 
-        _import_data = import_data
+        _import_data = copy.deepcopy(import_data)
         _import_data[CONF_PASSWORD] = "*****"
-        _LOGGER.debug("Starting import of sensor from configuration.yaml - %s", _import_data)
         if import_data is None:
             _LOGGER.error(import_data)
             return self.async_abort(reason="No configuration to import.")
@@ -138,13 +156,14 @@ class IstaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         # Verarbeite die importierten Konfigurationsdaten weiter
         import_data[CONF_URL] = "de_url"
         import_data[CONF_UPDATE_INTERVAL] = 24
+        import_data[CONF_MFA] = ""
+        _LOGGER.debug("Starting import of sensor from configuration.yaml - %s", _import_data)
         return await self.async_step_german(import_data)
 
 
 def validate_options_input(user_input: dict[str, Any]) -> dict[str, str]:
-    """Validate the user input allows us to connect.
-    Data has the keys from DATA_SCHEMA with values provided by the user.
-    """
+    """Validate the user input allows us to connect. Data has the keys from DATA_SCHEMA with values provided by the user."""
+
     errors = {}
     if CONF_URL not in user_input or user_input[CONF_URL] != "de_url":
         errors["base"] = "not_allowed"
